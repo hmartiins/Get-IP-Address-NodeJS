efa474eaac7ab0fd00406653a1e684cb518d5401
hmartiins <hmartins224@gmail.com>
2021-11-24 19:41:22 -0300

    ..

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.get('/',function(request, response) {
   response.json({"ip": clientIp});
 });
   
-app.listen(PORT || 3000, () => console.log(`App listening on port 3000`))
+app.listen(process.env.PORT || 3000, () => console.log(`App listening on port 3000`))
