00951f2058fa8c3741730dd3c4e19075cf062490
hmartiins <hmartins224@gmail.com>
2021-11-24 19:24:24 -0300

    update method

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
   
 app.get('/',function(request, response) {
-  const ipAddress = request.socket.remoteAddress;
-  response.json({"ip": ipAddress});
+    var idAddress = request.header('x-forwarded-for') || request.connection.remoteAddress;
+    response.json({"ip": idAddress});
 });
   
-app.listen(3000, () => console.log(`Started!`))
+app.listen(3000, () => console.log(`App listening on port 3000`))
